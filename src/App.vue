085720acf894ac47d7897c1f9a67d94a03394108
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-06-14 23:37:24 -0300

    refactor: simplify App.vue structure and remove unused base.css

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,7 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header
-    class="flex flex-col items-center gap-8 py-8 md:flex-row md:items-center md:justify-start md:gap-8 md:pr-8"
-  >
-    <img
-      alt="Vue logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-      class="block mx-auto md:mx-0"
-    />
-
-    <div class="flex flex-col items-start flex-wrap">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div><h1 class="text-blue-500 text-2xl font-bold">Bem-vindo ao Vue + Tailwind!</h1></div>
 </template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
