0dba88f4d6c9b2c559a28a94d1dd0f33aac5edd7
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-06-17 19:13:28 -0300

    feat: implement landing layout and login page, update router configuration

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,5 @@
 <template>
-  <div class="flex flex-col h-screen">
-    <header class="flex items-center h-14 px-4 border-b border-gray-300 sm:h-16 md:px-6 lg:px-8">
-      <div>
-        <a class="flex items-center gap-2 font-semibold" href="#">
-          <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="40" height="40" />
-        </a>
-      </div>
-      <nav class="ml-auto space-x-4 flex items-center h-10 sm:space-x-6">
-        <RouterLink :to="{ name: 'home' }">Início</RouterLink>
-        <RouterLink to="/features">Diferenciais</RouterLink>
-        <RouterLink to="/pricing">Preços</RouterLink>
-        <RouterLink to="/contact">Contato</RouterLink>
-      </nav>
-    </header>
-
-    <main class="flex-1 flex items-center justify-center">
-      <RouterView />
-    </main>
-
-    <footer class="flex items-center h-14 px-4 border-t border-gray-300 sm:h-16 md:px-6 lg:px-8">
-      <p class="flex-1 text-sm text-gray-500 text-center">
-        © {{ currentYear }} Capsule Corporation. Todos os direitos reservados
-      </p>
-    </footer>
-  </div>
+  <RouterView />
 </template>
 
-<script setup lang="ts">
-const currentYear = new Date().getFullYear()
-</script>
+<script setup lang="ts"></script>
