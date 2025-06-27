6e8a5e3343f3af78f460079d6aefa488b0fef085
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-06-26 22:46:03 -0300

    feat: update LoginPage localization and improve accessibility with translated labels

--- src/modules/auth/pages/LoginPage.vue
+++ src/modules/auth/pages/LoginPage.vue
@@ -2,16 +2,16 @@
   <div class="bg-gray-100 flex justify-center items-center h-screen">
     <div class="w-1/2 h-screen hidden lg:block">
       <img
-        src="https://placehold.co/800x/667fff/ffffff.png?text=Your+Image&font=Montserrat"
-        alt="Placeholder Image"
+        src="https://images.pexels.com/photos/28396251/pexels-photo-28396251.jpeg"
+        alt="Imagem de Login"
         class="object-cover w-full h-full"
       />
     </div>
     <div class="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
-      <h1 class="text-2xl font-semibold mb-4">Login</h1>
+      <h1 class="text-2xl font-semibold mb-4">Entrar</h1>
       <form action="#" method="POST">
         <div class="mb-4">
-          <label for="username" class="block text-gray-600">Username</label>
+          <label for="username" class="block text-gray-600">Nome de Usuário</label>
           <input
             type="text"
             id="username"
@@ -21,7 +21,7 @@
           />
         </div>
         <div class="mb-4">
-          <label for="password" class="block text-gray-600">Password</label>
+          <label for="password" class="block text-gray-600">Senha</label>
           <input
             type="password"
             id="password"
@@ -32,20 +32,20 @@
         </div>
         <div class="mb-4 flex items-center">
           <input type="checkbox" id="remember" name="remember" class="text-blue-500" />
-          <label for="remember" class="text-gray-600 ml-2">Remember Me</label>
+          <label for="remember" class="text-gray-600 ml-2">Lembrar de mim</label>
         </div>
         <div class="mb-6 text-blue-500">
-          <a href="#" class="hover:underline">Forgot Password?</a>
+          <a href="#" class="hover:underline">Esqueceu a senha?</a>
         </div>
         <button
           type="submit"
           class="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full"
         >
-          Login
+          Entrar
         </button>
       </form>
       <div class="mt-6 text-blue-500 text-center">
-        <a href="#" class="hover:underline">Sign up Here</a>
+        <a href="#" class="hover:underline">Cadastre-se aqui</a>
       </div>
     </div>
   </div>
